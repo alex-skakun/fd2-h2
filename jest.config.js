998995b51554489b83abf55dca941a9742319c45
Grishkevich <kdarua4@gmail.com>
2023-01-30 17:13:15 +0300

    fd2-h2/Grishkevich

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-    testEnvironment: 'jsdom'
-};
+// module.exports = {
+//     testEnvironment: 'jsdom'
+// };
+
+
+ function calc (numder1, number2, operation) {
+    if ( operation ==='add') { 
+    operation = numder1 + number2;
+    } else if ( operation === 'subtract') {
+    operation = numder1 - number2;
+    } else if ( operation === 'multiply') {
+    operation = numder1 * number2;
+    } else if ( operation === 'divide') {
+    operation = numder1 / number2;
+    }
+    return operation;
+}
+console.log (calc(4, 6, 'divide'));
