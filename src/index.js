118bc63154fae5e80418c899580795c65758c0df
Pavel <uscharov@mail.ru>
2023-01-30 20:28:54 +0300

    refactoring code

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ function summ(){
 }
  
 // функция cals 
-let calc = function(){
+ function calc(){
     let [numOne, numTwo] = arguments;
     // возвращаемая функция вычислений
     return function resolve(action){  
@@ -57,30 +57,30 @@ let calc = function(){
 
  
 
-console.log(calc(10, 5)('Add'));
-console.log(calc(10, 5)('ADD'));
-console.log(calc(10, 5)('Subtract'));
-console.log(calc(10, 5)('SUBTRACT'));
-console.log(calc(10, 5)('Multiply'));
-console.log(calc(10, 5)('MULTIPLY'));
-console.log(calc(10, 5)('Divide'));
-console.log(calc(10, 5)('DIVIDE'));
-console.log(calc(10, 5)('unknown'));
+// console.log(calc(10, 5)('Add'));
+// console.log(calc(10, 5)('ADD'));
+// console.log(calc(10, 5)('Subtract'));
+// console.log(calc(10, 5)('SUBTRACT'));
+// console.log(calc(10, 5)('Multiply'));
+// console.log(calc(10, 5)('MULTIPLY'));
+// console.log(calc(10, 5)('Divide'));
+// console.log(calc(10, 5)('DIVIDE'));
+// console.log(calc(10, 5)('unknown'));
 
-const result = calc(1, 2);
-console.log(typeof result);
-console.log(typeof result, "toBe('function')");
-const result2 = calc('1', true);
-console.log(typeof result2);
-console.log(typeof result2, "toBe('function')");
-console.log(calc(10, 5)('add'), 'toBe(15)');
-console.log(calc('10', 5)('add'), 'toBeNull'); 
-console.log(calc(10, 5)('subtract'), 'toBe(`5)'); 
-console.log(calc('10', 5)('subtract'), 'toBeNull'); 
-console.log(calc(10, 5)('multiply'), 'toBe(50)'); 
-console.log(calc('10', 5)('multiply'), 'toBeNull()'); 
-console.log(calc(10, 5)('divide'), 'toBe(2)'); 
-console.log(calc('10', 5)('divide'), 'toBeNull'); 
+// const result = calc(1, 2);
+// console.log(typeof result);
+// console.log(typeof result, "toBe('function')");
+// const result2 = calc('1', true);
+// console.log(typeof result2);
+// console.log(typeof result2, "toBe('function')");
+// console.log(calc(10, 5)('add'), 'toBe(15)');
+// console.log(calc('10', 5)('add'), 'toBeNull'); 
+// console.log(calc(10, 5)('subtract'), 'toBe(`5)'); 
+// console.log(calc('10', 5)('subtract'), 'toBeNull'); 
+// console.log(calc(10, 5)('multiply'), 'toBe(50)'); 
+// console.log(calc('10', 5)('multiply'), 'toBeNull()'); 
+// console.log(calc(10, 5)('divide'), 'toBe(2)'); 
+// console.log(calc('10', 5)('divide'), 'toBeNull'); 
  
   
  
