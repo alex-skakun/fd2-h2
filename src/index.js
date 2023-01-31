b833a46dd06f85a67da291c5683c86eb86d25e03
AlexanderHanziuk <aleksandrtiskov488@gmail.com>
2023-01-31 10:40:36 +0300

    update index.js-2

--- src/index.js
+++ src/index.js
@@ -2,30 +2,30 @@
 
 function calc(firstNumber, secondNumber) {
 	return function doActions(option) {
-	let result;
-	if (typeof firstNumber === 'number' && typeof secondNumber === 'number') {
-		switch (option) {
-			case 'add':
-				result = firstNumber + secondNumber;
-				break;
-			case 'subtract':
-					result = firstNumber - secondNumber;
-				break;
-			case 'multiply':
-					result = firstNumber * secondNumber;
-				break;
-			case 'divide':
-					result = firstNumber / secondNumber;
-				break;
-			default:
-					result = null;
-				break;
+		let result;
+		if (typeof firstNumber === 'number' && typeof secondNumber === 'number') {
+			switch (option) {
+				case 'add':
+					result = firstNumber + secondNumber;
+					break;
+				case 'subtract':
+						result = firstNumber - secondNumber;
+					break;
+				case 'multiply':
+						result = firstNumber * secondNumber;
+					break;
+				case 'divide':
+						result = firstNumber / secondNumber;
+					break;
+				default:
+						result = null;
+					break;
+			}
+		} else {
+				result = null;
 		}
-	} else {
-		result = null;
+			return result;
 	}
-	return result;
-}
 }
 
 
