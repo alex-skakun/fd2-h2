29590c935db80e1eec04f17589bba72ee0da28bd
Chertovich Sergei <chertovich-1984@tut.by>
2023-02-01 21:52:58 +0300

    Сергей Чертович

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 
 function calc(num1, num2) {
     
-    if (num1 !== NaN && num2 !== NaN) {
-        
-        return function (performance) {
+    return function (performance) {
+
+        if ( typeof num1 === "number"  && typeof num2 === "number") {
 
             switch (performance) {
                 
                 case 'add': return  num1 + num2;
                     break;
-                case 'subtract': return num2 - num1;
+                case 'subtract': return num1 - num2;
                     break;
                 case 'multiply': return num1 * num2;
                     break;
@@ -17,8 +17,9 @@ function calc(num1, num2) {
                     break;
                 default: return null; 
             }
-        
-        }
 
-    }  return null;
+        } return null;
+    
+    }
+
 }
